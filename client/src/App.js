7a67aaaf6800cad3623d1878cba891065fd8cbce
John Young <john@codeandcreative.com>
2021-02-10 11:06:11 -0800

    Adds comments about socket implementation

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,24 @@ import { io } from "socket.io-client";
 
 var socket = io();
 function App() {
+  // The messages from the socket will be held in an array in a
+  // state variable.
   const [messages, setMessages] = useState([]);
 
+  /**
+   * This sets up an event listener, listening for 'chat message' events broadcasted
+   * from the socket hosted on the Express server.
+   */
   socket.on('chat message', function(msg) {
+
+    // this sets the messages state to include the new message
     setMessages([...messages, msg]);
   });
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+
+      {/* Clicking this button emits a 'chat message' event to the socket hosted on the Express server, sending the string "Hi there" */}
       <button onClick={() => socket.emit('chat message', "Hi there")}>Click me</button>
       <div>{messages && messages.map(msg => <p>{msg}</p>)}</div>
     </div>
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const path = require("path");
+
+// If the runtime environment has a PORT environment variable, use that 
+// to establish our host port. If not, use 3001.
 const PORT = process.env.PORT || 3001;
+
+// Create an Express app
 const app = express();
 
+// Assign that Express app to be a handler to an http server 
 const http = require('http').createServer(app);
+
+// This will create a socket io instance that can also utilize
+// the http service. socket.io will try to use WebSockets, which 
+// is a different protocol (ws) than http. It will use the 
+// same port as the http service. If WebSockets are unavailable 
+// due to an old browser, socket.io will try to use AJAX polling
+// from the client to see if there are any new socket events
+// @see https://stackoverflow.com/questions/10112178/differences-between-socket-io-and-websockets
 const io = require('socket.io')(http);
 
 // Serve up static assets (usually on heroku)
@@ -11,15 +25,25 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Socket.io event handler listening for
+// client connections. When a client connects,
+// a realtime socket between the client and the server
+// is opened using WebSockets
 io.on('connection', (socket) => {
   console.log('a user connected');
   
+  // when the socket is closed, e.g., when a web page closes or is refreshed
+  // a socket disconnect event happens
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
+  // listening for 'chat message' events
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
+
+    // broadcast the new msg to those clients listening for 'chat message'
+    // events
     io.emit('chat message', msg);
   });
 });
@@ -31,6 +55,7 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Start listening for requests on the PORT established at the top
 http.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
