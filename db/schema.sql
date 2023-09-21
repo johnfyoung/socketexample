f68524d5d72c21bb8ae0f7cc7a06d76ad1767c84
John Young <jyoung@instructirs.2u.com>
2023-09-20 20:35:06 -0700

    Adds Week 14 folder structure

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS chat_app;
+CREATE DATABASE chat_app;
--- server.js
+++ server.js
@@ -1,35 +1,71 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// If the runtime environment has a PORT environment variable, use that 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// If the runtime environment has a PORT environment variable, use that
 // to establish our host port. If not, use 3001.
 const PORT = process.env.PORT || 3001;
 
 // Create an Express app
 const app = express();
 
-// Assign that Express app to be a handler to an http server 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Assign that Express app to be a handler to an http server
 const http = require('http').createServer(app);
 
 // This will create a socket io instance that can also utilize
-// the http service. socket.io defaults to using long polling over the 
+// the http service. socket.io defaults to using long polling over the
 // http connection and tries to upgrade the connection to use
-// WebSockets, which is a different protocol (ws) than http. It uses the 
+// WebSockets, which is a different protocol (ws) than http. It uses the
 // same port as the http service.
 //
-// To ensure we use websockets, we have set the order of the transport to 
+// To ensure we use websockets, we have set the order of the transport to
 // prefer the websocket transport
-// 
+//
 // @see https://stackoverflow.com/questions/10112178/differences-between-socket-io-and-websockets
 // @see https://github.com/socketio/socket.io-client/issues/883
 const io = require('socket.io')(http, {
-  transports: ['websocket', 'polling']
+  transports: ['websocket', 'polling'],
 });
 
-// Serve up static assets (usually in production on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Socket.io event handler listening for
 // client connections. When a client connects,
@@ -37,7 +73,7 @@ if (process.env.NODE_ENV === "production") {
 // is opened using WebSockets
 io.on('connection', (socket) => {
   console.log('Socket connected: ', socket.id);
-  
+
   // when the socket is closed, e.g., when a web page closes or is refreshed
   // a socket disconnect event happens
   socket.on('disconnect', () => {
@@ -47,25 +83,19 @@ io.on('connection', (socket) => {
   // listening for 'chat message' events
   socket.on('chat message', (msg, fn) => {
     console.log('message: ' + msg);
-    if(!msg.username) {
-      msg.username = "Anonymous";
+    if (!msg.username) {
+      msg.username = 'Anonymous';
     }
-    fn("Got your message");
+    fn('Got your message');
     // broadcast the new msg to those clients listening for 'chat message' events
     // The broadcast object on this socket doesn't send the message back to its destination
     socket.broadcast.emit('chat message', msg);
   });
 });
 
-// When in production, send every unhandled request to the React app by refreshing the public index.html
-// When in development, the webpack dev server catches these requests at localhost:3000
-// Note: define any API routes before this runs
-app.get("*", function(req, res) {
-  console.log("Got a request for the client,eh?...probably handled by React Router");
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(routes);
 
 // Start listening for requests on the PORT established at the top
-http.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  http.listen(PORT, () => console.log(`🌎 ==> Server now on port ${PORT}!`));
 });
